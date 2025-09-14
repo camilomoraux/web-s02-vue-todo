452bd01231473ca7b30554e2b808e3b8ba69074b
camilomoraux <124800178+camilomoraux@users.noreply.github.com>
2025-09-14 23:20:40 +0000

    Contador Sesión 2

--- src/assets/sesion2_contador.js
+++ src/assets/sesion2_contador.js
@@ -1,1 +1,84 @@
+/*
 console.log('Hola desde sesion2_contador.js')
+
+let usuarios = [
+  'Bruno', 
+  'Camilo', 
+  'Mariana',
+  'Ana', 
+  'Luis', 
+  'Sofía', 
+  'Diego', 
+  'Valeria', 
+  'Jorge', 
+  'Fernanda',
+  'Andrés', 
+  'Claudia', 
+  'Mateo', 
+  'Paula', 
+  'Ricardo', 
+  'Daniela',
+  'Santiago', 
+  'Gabriela', 
+  'Felipe', 
+  'Laura'
+];
+
+let decirHola = function(nombre) {
+  console.log('Hola ' + nombre)
+}
+
+/* versión 1.0
+
+for (let i = 0; i < usuarios.length; i++) {
+  decirHola(usuarios[i])
+}
+
+
+
+usuarios.forEach(nombre => {
+  decirHola(nombre) 
+});
+
+let number = 1;
+
+
+while(number <= 4) { 
+  console.log(number)
+  number++
+}
+
+*/
+
+let count = 0;
+console.log(count)
+
+let add = function() {
+  count++
+  console.log(count)
+  updateValue()
+}
+
+let less = function() {
+  count--
+  console.log(count)
+  updateValue()
+}
+
+let updateValue = function() {
+  value.innerText = count
+}
+
+let addButton = document.getElementById('increment');
+
+addButton.addEventListener('click', () => { 
+  add();
+})
+
+let lessButton = document.getElementById('decrement');
+
+lessButton.addEventListener('click', () => { 
+  less();
+})
+
+let value = document.getElementById('value');
